65e080506ee43a4ca45d632828efeb02bc7aebf5
huseynaktas <h.akts1007@gmail.com>
2024-05-14 23:43:12 +0300

    Appwrite Authentication's Section Part 1

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,7 +24,7 @@ import { authformSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import SignUp from '@/app/(auth)/sign-up/page'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
 
 
 
@@ -57,12 +57,12 @@ const AuthForm = ({type}: {type: string}) => {
       }
 
       if(type === 'sign-in'){
-        const response = await signIn({
-          email: data.email,
-          password: data.password,
-        })
+        // const response = await signIn({
+        //   email: data.email,
+        //   password: data.password,
+        // })
 
-        if(response) router.push('/')
+        // if(response) router.push('/')
       }
     } catch (error) {
       console.log(error)
