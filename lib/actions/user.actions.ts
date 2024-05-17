0d3b0c5589bbce45b1713ed5899689b08fac0d34
huseynaktas <h.akts1007@gmail.com>
2024-05-17 23:08:32 +0300

    Appwrite Authentication's Section Part 2

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,8 +5,12 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({email, password}: signInProps) => {
     try {
+        const { account } = await createAdminClient();
+        const response = await account.createEmailPasswordSession(email, password);
+
+        return parseStringify(response);
         
     } catch (error) {
         console.error('Error', error)
@@ -55,3 +59,14 @@ export async function getLoggedInUser() {
     }
   }
   
+
+export const logoutAccount = async () => {
+    try {
+        const {account} = await createSessionClient();
+        cookies().delete('appwrite-session');
+
+        await account.deleteSession('current');
+    } catch (error) {
+        return null;
+    }
+}
